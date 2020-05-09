cade80846ae9f51517d39d47590597caf1c32873
Kevin Mangkornkaew <kem@morningtrain.dk>
2020-05-09 12:57:04 +0200

    refactor mainpage

---
+++ netflixRoulette/netflixRoulette/Models/DiscoverPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace netflixRoulette.Models
+{
+    public class Example
+    {
+        public int page { get; set; }
+        public int total_results { get; set; }
+        public int total_pages { get; set; }
+        public IList<Result> results { get; set; }
+    }
+}
